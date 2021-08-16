2708b494b378bc70ce5805d5e8311c3de6eb99e7
Jan shair <janshair@tapdevs.com>
2021-08-16 17:51:05 +0100

    Added more functions

--- src/Functions.java
+++ src/Functions.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class Functions {
 
+    /**
+     * 1. Void
+     * 2. Void with parameteres
+     * 3. Return type
+     * 4. Return type with parameters
+     * @param args
+     */
 
     public static void main(String[] args){
         System.out.println();
+
         int userInput = getUserInput();
         System.out.println("User entered " + userInput);
         int fictorial = calculateFictorial(userInput);
         printFictorialValue(fictorial);
+        printEndOfProgrammeMessage();
+        int y = findValueOfY(10)
+        findValueOfX(y);
+
+    }
+
+    private static void printEndOfProgrammeMessage() { // without parameter
+        System.out.println("Good bye");
     }
 
     private static void printFictorialValue(int fictorial) { // with parameter only
@@ -24,6 +40,15 @@ public class Functions {
         return product;
     }
 
+    private static int findValueOfY(int z) {
+        int y = 10 + 3*z;
+        return y;
+
+    }
+    private static int findValueOfX(int y) {
+        int x = 12 + 2 * y;
+        return x;
+    }
     private static int getUserInput() { // without parameter and return type
         Scanner scan = new Scanner(System.in);
         int input = scan.nextInt();
